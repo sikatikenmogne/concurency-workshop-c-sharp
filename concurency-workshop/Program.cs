0c5159e2324d89e20f068482c54d98065c5f46a3
Sikati Kenmogne Samuel <71256717+sikatikenmogne@users.noreply.github.com>
2024-05-02 17:31:11 +0100

    Ajoutez des fichiers projet.

---
+++ concurency-workshop/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concurency_workshop
+{
+    delegate int MyDelegateMethod(int i1, int i2);
+    delegate int MySecondDelegateMethod(int i2);
+
+    internal class Program
+    {
+        private static Func<int, int> square;
+
+        public static int addMethod(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            // Let the method 'int method (int v1, int v2)'. This method adds two values and returns the result. Write the delegate who will invoke this method
+            MyDelegateMethod myDelegateMethod = addMethod;
+
+            int result = myDelegateMethod(1, 2);
+
+            Console.WriteLine(result);
+            // ---------------
+
+            // Construct using a lambda expression a method that will calculate the square of a number
+            square = (int i) => { return i * i; };
+
+            result = square(2);
+
+            // TODO: C'est quoi la différence entre un Functeur (Func<int, int>) et les delegates
+            Console.WriteLine(result);
+
+            Console.WriteLine("or");
+            // or
+
+            MySecondDelegateMethod mySecondDelegateMethod = (int i) => { return i * i; };
+
+            result = mySecondDelegateMethod(2);
+
+            Console.WriteLine(result);
+            // ---------------
+
+            // Implement an anonymous type that has an 'int', a 'string'. Exposing its use
+            var johnDo = new {name = "unknow", id = 1 };
+
+            Console.WriteLine(johnDo.name);
+            Console.WriteLine(johnDo.name.GetType());
+            Console.WriteLine("-------------------");
+            Console.WriteLine(johnDo.id);
+            Console.WriteLine(johnDo.id.GetType());
+            // ---------
+        }
+    }
+}
